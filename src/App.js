ec017b86062de12ae702e61759428254a6ef17e7
anilkumar23Sap <anikumar23@publicisgroupe.net>
2020-05-02 11:22:25 +0530

    conditional rendering

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ConditionalScreenBlock from './ConditionaBlock-Screen-Size/CondtionalScreen';
+
+function App() {
+  return (
+    <div className="App">
+ 
+    <ConditionalScreenBlock/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/util/util.js
@@ -0,0 +1,23 @@
+
+export const DEVICES={
+    desktop:'Desktop',
+    mobile:'Mobile'
+
+}
+
+//calculate screen size
+
+
+export const  getScreenSize =() =>{
+    const width = window.innerWidth;
+    if(width > 760){
+        return  DEVICES.desktop;
+        }
+     else if(width < 760){
+         return DEVICES.mobile;
+     }
+     else{
+         return DEVICES.desktop;
+     }
+
+}
